2bb04405f6c0f1bb40fa0fcabab837d3b95a74c9
hyum's house <vina1601@ewhain.net>
2023-10-27 19:14:33 +0900

    #10.27 BOJ

---
+++ BOJ Algorithm/algorithm2.py
@@ -0,0 +1,13 @@
+numX = int(input())
+numY = int(input())
+
+if (numX > 0):
+    if (numY > 0):
+        print(1)
+    else:
+        print(4)
+else:
+    if (numY > 0):
+        print(2)
+    else:
+        print(3)
